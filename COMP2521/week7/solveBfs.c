573f5ce42c3e41bae360fd2315933ec4707cc1e5
z5165158 <jake.edwards1@student.unsw.edu.au>
2021-11-03 11:05:24 +1100

    finished task 1 and 2 lab 7 added ass2 to repo

--- COMP2521/week7/solveBfs.c
+++ COMP2521/week7/solveBfs.c
@@ -9,6 +9,57 @@
 #include "helpers.h"
 #include "Maze.h"
 #include "Queue.h"
+typedef enum {
+    NONE,
+    VISITED,
+    PATH,
+} State;
+
+bool isvalid(Maze m,Cell V){
+    int Height = MazeHeight(m);
+    int Width = MazeWidth(m);
+    //if within bounds
+    if(V.row < Height && V.row >= 0 && V.col < Width && V.col >= 0 && MazeIsWall(m,V) == false){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+bool adjacent(Maze m,Cell V,Cell W){
+    Cell Left = {V.row,V.col-1};
+    Cell Right = {V.row,V.col+1};
+    Cell Up = {V.row+1,V.col};
+    Cell Down = {V.row-1,V.col};
+    if(isvalid(m,W) == true && isvalid(m,V)){
+        if(isvalid(m,Left) == true){
+            if(Left.row == W.row && Left.col == W.col){
+                return true;
+            }
+        }
+        if(isvalid(m,Right) == true){
+            if(Right.row == W.row && Right.col == W.col){
+                return true;
+            }
+        }
+        if(isvalid(m,Up) == true){
+            if(Up.row == W.row && Up.col == W.col){
+                return true;
+            }
+        }
+        if(isvalid(m,Down) == true){
+            if(Down.row == W.row && Down.col == W.col){
+                return true;
+            }
+        }
+    }
+        return false;
+}
+Cell ReturnCell(int row,int col){
+    Cell x ={.row = row,.col = col};
+    return x;
+}
 
 bool solve(Maze m) {
     // TODO: Complete this function
@@ -38,25 +89,45 @@ bool solve(Maze m) {
 |  end if
 */
 bool found = false;
+bool **boolmatrix = createBoolMatrix(MazeHeight(m), MazeWidth(m));
+Cell **predecessormatrix = createCellMatrix(MazeHeight(m), MazeWidth(m));
 Cell Start = MazeGetStart(m);
 Cell V;
-MazeVisit(Start);
+Cell W;
+
+boolmatrix[Start.row][Start.col] = VISITED;
 Queue Q = QueueNew(); //rmb to free
 QueueEnqueue(Q,Start);
 
-while(found == false && QueueSize(Q) > 0){
+while(!QueueIsEmpty(Q)){
     V = QueueDequeue(Q);
-    if(MazeVisit(V) == true){
+    
+    if(MazeVisit(m,V) == true){
+        while(V.row != Start.row || V.col != Start.col){
+            MazeMarkPath(m,V);
+            V   = predecessormatrix[V.row][V.col];
+        }
+        MazeMarkPath(m,V);
+        MazeMarkPath(m,Start); 
         found = true;
+        return found;
     }
     else{
-        for(int row = 0;row < MazeWidth(m);row++){
-            for(int col = 0;col < MazeHeight(m); col++){
-                Cell x = {row,col};
-                if(visited = None && MazeIsWall(m,C) == false){
-                    MazeVisit(V);
-                    //when do we mark part of  the path
-                    enqueue vertex into Q
+        for(int row = 0;row < MazeHeight(m);row++){
+            for(int col = 0;col < MazeWidth(m); col++){
+                
+                W = ReturnCell(row,col);
+                
+                if(boolmatrix[W.row][W.col] == NONE && adjacent(m,V,W) == true){// && W.row != Start.row && W.col != Start.col){
+                    
+                    predecessormatrix[W.row][W.col] = V;
+                    MazeVisit(m,W);
+                    boolmatrix[W.row][W.col] = VISITED;
+                    
+                    
+                    
+                    QueueEnqueue(Q,W);
+                    
                 }
                 
             }
@@ -69,6 +140,34 @@ while(found == false && QueueSize(Q) > 0){
 }
 
 
+// bool found = false;
+// Cell v;
+// bool **boolmatrix = createBoolMatrix(MazeHeight(m), MazeWidth(m));
+
+// for (v = 0; v < nV; v++)
+//       visited[v] = -1;
+
+//    visited[src] = src;
+//    queue Q = newQueue();
+//    QueueEnqueue(Q, src);
+//    while (!QueueIsEmpty(Q)) {
+//       v = QueueDequeue(Q);
+//       Vertex w;
+//       for (w = 0; w < nV; w++)
+// 	 if (adjacent(g, v, w) && visited[w] == -1) {
+// 	    visited[w] = v;
+// 	    if (w == dest)
+// 	       return true;
+// 	    else
+// 	       QueueEnqueue(Q, w);
+// 	 }
+//    }
+//    return false;
+// }
+
+    freeCellMatrix(predecessormatrix);
+    freeBoolMatrix(boolmatrix);
+    QueueFree(Q);
     return found;
 }
 
--- COMP2521/week7/solveDfs.c
+++ COMP2521/week7/solveDfs.c
@@ -9,10 +9,155 @@
 #include "helpers.h"
 #include "Maze.h"
 #include "Stack.h"
+typedef enum {
+    NONE,
+    VISITED,
+    PATH,
+} State;
+
+bool isvalid(Maze m,Cell V){
+    int Height = MazeHeight(m);
+    int Width = MazeWidth(m);
+    //if within bounds
+    if(V.row < Height && V.row >= 0 && V.col < Width && V.col >= 0 && MazeIsWall(m,V) == false){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+bool adjacent(Maze m,Cell V,Cell W){
+    Cell Left = {V.row,V.col-1};
+    Cell Right = {V.row,V.col+1};
+    Cell Up = {V.row+1,V.col};
+    Cell Down = {V.row-1,V.col};
+    if(isvalid(m,W) == true && isvalid(m,V)){
+        if(isvalid(m,Left) == true){
+            if(Left.row == W.row && Left.col == W.col){
+                return true;
+            }
+        }
+        if(isvalid(m,Right) == true){
+            if(Right.row == W.row && Right.col == W.col){
+                return true;
+            }
+        }
+        if(isvalid(m,Up) == true){
+            if(Up.row == W.row && Up.col == W.col){
+                return true;
+            }
+        }
+        if(isvalid(m,Down) == true){
+            if(Down.row == W.row && Down.col == W.col){
+                return true;
+            }
+        }
+    }
+        return false;
+}
+Cell ReturnCell(int row,int col){
+    Cell x ={.row = row,.col = col};
+    return x;
+}
+/*
+dfsPathCheck(G,v,dest):
+|  visited = visited U {v}
+|  for all (v,w) in edges(G) do
+|  |  if w=dest then   // found edge to dest
+|  |     return true
+|  |  else if w not in visited then
+|  |     if dfsPathCheck(G,w,dest) then
+|  |        return true  // found path via w to dest
+|  |     end if
+|  |  end if
+|  end for
+|  return false  // no path from v to dest
+*/
+
+bool dfsPathCheck(Maze m,Cell V,Cell **predecessormatrix,bool **boolmatrix){//,int *DestRow, int* DestCol){
+    MazeVisit(m,V);
+    boolmatrix[V.row][V.col] = VISITED;
+    Cell W;
+    for(int row = 0;row < MazeHeight(m);row++){
+            for(int col = 0;col < MazeWidth(m); col++){
+                
+                W = ReturnCell(row,col);
+                if(adjacent(m,V,W) == true){
+                    
+
+                    //if destination
+                    if(MazeVisit(m,W) == true){
+                        MazeMarkPath(m,W);
+                        predecessormatrix[W.row][W.col] = V;
+                        Cell Start = MazeGetStart(m);
+                        while(W.row != Start.row || W.col != Start.col){
+                            MazeMarkPath(m,W);
+                            W = predecessormatrix[W.row][W.col];
+                        }
+
+                        MazeMarkPath(m,Start); 
+                        return true;
+                    }
+                    //if not visited
+                    else if(boolmatrix[W.row][W.col] == NONE){
+                        predecessormatrix[W.row][W.col] = V;
+                        if(dfsPathCheck(m,W,predecessormatrix,boolmatrix) == true){
+                            return true;
+                        }
+                        
+
+                    }
+                    
+                    
+
+                }
+            }
+    }
+    
+
+    return false;
+}
+// Cell FindDest(Maze m){
+//     Cell W;
+//     for(int row = 0;row < MazeHeight(m);row++){
+//             for(int col = 0;col < MazeWidth(m); col++){
+                
+//                 W = ReturnCell(row,col);
+//                 if(MazeVisit(m,W) == true){
+//                     return W;
+//                 }
+//             }
+//     }
+//     return ReturnCell(-1,-1);
+
+// }
 
 bool solve(Maze m) {
     // TODO: Complete this function
     //       Feel free to add helper functions
-    return false;
+    /*
+
+    hasPath(G,src,dest):
+|  Input  graph G, vertices src,dest
+|  Output true if there is a path from src to dest,
+|         false otherwise
+|
+|  return dfsPathCheck(G,src,dest)
+    */
+
+    bool found = false;
+    bool **boolmatrix = createBoolMatrix(MazeHeight(m), MazeWidth(m));
+    Cell **predecessormatrix = createCellMatrix(MazeHeight(m), MazeWidth(m));
+    Cell Start = MazeGetStart(m);
+
+    found = dfsPathCheck(m,Start,predecessormatrix,boolmatrix);
+    
+    
+    
+
+    freeCellMatrix(predecessormatrix);
+    freeBoolMatrix(boolmatrix);
+    return found;
 }
 
